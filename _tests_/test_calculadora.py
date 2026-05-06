48680132ae6e24cd51cc1248f7226043937832d6
aamorim1705-a11y <aamorim1705@gmail.com>
2026-05-06 16:50:56 +0100

    Criamos as 4 operações básicas e testes de unidade simples e data driven

--- _tests_/test_calculadora.py
+++ _tests_/test_calculadora.py
@@ -3,6 +3,7 @@ import pytest # framework de teste de unidade
 
 # funções que serão testadas
 from calculadora.calculadora import somar_dois_numeros, subtrair_dois_numeros, multiplicar_dois_numeros, dividir_dois_numeros
+from utils.utils import ler_csv # função de leitura de arquivo csv
 
 # 2 - Testes
 def test_somar_dois_numeros():
@@ -14,31 +15,6 @@ def test_somar_dois_numeros():
     num1 = 5
     num2 = 7
     resultado_esperado = 12
-
-   # Act / Executa
-    resultado_obtido = somar_dois_numeros(num1, num2)
-
-   # Assert / Valida
-    assert resultado_esperado == resultado_obtido
-
-# Test Baseados em Dados = Data Driven Tests (DDT) --» Massa de Teste
-   # Dados em uma lista
-   # Dados em um arquivo, vários formatos: csv (texto separado por vírgula), json, xml, dat
-
-@pytest.mark.parametrize('num1, num2, resultado_esperado',
-                         [ # array / matriz
-                            (5, 7, 12), # tupla / registro
-                            (0, 8, 8),
-                            (10, -15, -5),
-                            (6, 0.75, 6.75)
-                         ]
-                        )
-def test_somar_dois_numeros_lista(num1, num2, resultado_esperado):
-
-    # Padrão / Standard AAA (Triple A / 3A) IEEE = Arrange, Act, Assert
-
-    # Arrange / Prepara / Configura
-    # Dados de entrada e saída fornecidos pela massa de teste em formato de lista
 
    # Act / Executa
     resultado_obtido = somar_dois_numeros(num1, num2)
@@ -93,4 +69,46 @@ def test_dividir_por_zero():
 
     # Valida
     assert resultado_esperado == resultado_obtido
+
+# Test Baseados em Dados = Data Driven Tests (DDT) --» Massa de Teste
+   # Dados em uma lista
+   # Dados em um arquivo, vários formatos: csv (texto separado por vírgula), json, xml, dat
+
+@pytest.mark.parametrize('num1, num2, resultado_esperado',
+                         [ # array / matriz
+                            (5, 7, 12), # tupla / registro
+                            (0, 8, 8),
+                            (10, -15, -5),
+                            (6, 0.75, 6.75)
+                         ]
+                        )
+def test_somar_dois_numeros_lista(num1, num2, resultado_esperado):
+
+    # Padrão / Standard AAA (Triple A / 3A) IEEE = Arrange, Act, Assert
+
+    # Arrange / Prepara / Configura
+    # Dados de entrada e saída fornecidos pela massa de teste em formato de lista
+
+   # Act / Executa
+    resultado_obtido = somar_dois_numeros(num1, num2)
+
+   # Assert / Valida
+    assert resultado_esperado == resultado_obtido
+
+@pytest.mark.parametrize('num1, num2, resultado_esperado',
+                           ler_csv('./fixtures/massa_somar.csv')
+                         )
+
+def test_somar_dois_numeros_csv(num1, num2, resultado_esperado):
+
+    # Padrão / Standard AAA (Triple A / 3A) IEEE = Arrange, Act, Assert
+
+    # Arrange / Prepara / Configura
+    # Dados de entrada e saída fornecidos pela massa de teste em formato de lista
+
+   # Act / Executa
+    resultado_obtido = somar_dois_numeros(float(num1), float(num2))
+
+   # Assert / Valida
+    assert float(resultado_esperado) == resultado_obtido
 
